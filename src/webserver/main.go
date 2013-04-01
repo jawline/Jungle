f22a11447dc0e8f45f0cb0a27b97b8405a3a9ec5
jawline <blake_l@imclueless.co.uk>
2013-04-01 11:53:26 -0700

    Added optional path to the webserver

--- src/webserver/main.go
+++ src/webserver/main.go
@@ -5,9 +5,11 @@ import (
 	"log"
 	"net/http"
 	"io/ioutil"
+	"fmt"
 )
 
 var addr = flag.String("addr", ":80", "WebServer Service")
+var fileDirectory = ""
 
 func pageHandler(c http.ResponseWriter, req *http.Request) {
 
@@ -17,9 +19,10 @@ func pageHandler(c http.ResponseWriter, req *http.Request) {
 		path = req.URL.String()[1:]
 	}
 
-	b, err := ioutil.ReadFile(path)
+	b, err := ioutil.ReadFile(fileDirectory + path)
 
 	if err != nil {
+		fmt.Printf("Error: " + err.Error())
 		return
 	}
 
@@ -27,7 +30,10 @@ func pageHandler(c http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+
+	directoryFlag := flag.String("path", "./", "The path to the server files")
 	flag.Parse()
+	fileDirectory = *directoryFlag
 
 	http.HandleFunc("/", pageHandler)
 
